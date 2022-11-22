bbdc6e01f044a83daea157bdd8c5dfdb0aa90b61
mrinvo <huss.mostafa12@gmail.com>
2022-11-22 21:39:12 +0200

    fkdsjflk

--- app/Http/Controllers/dashboard/ClerkController.php
+++ app/Http/Controllers/dashboard/ClerkController.php
@@ -195,6 +195,7 @@ class ClerkController extends Controller
         $det->delete();
         $files = File::where('clerk_id',$id)->get();
         foreach($files as $file){
+            dd($file->file);
             unlink(realpath($file->file));
             $file->delete();
         }
