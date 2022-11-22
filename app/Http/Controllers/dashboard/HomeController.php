3cf759c0ee9606e68ee70c5026e064f074583858
mrinvo <huss.mostafa12@gmail.com>
2022-11-22 18:13:14 +0200

    jfsdlkf

--- app/Http/Controllers/dashboard/HomeController.php
+++ app/Http/Controllers/dashboard/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
     public function home(){
         $jops = Jop::all();
 
-        $new = Clerk::has('detail','family','file')
+        $new = Clerk::has('details')->has('files')->has('families')
         ->where('status','new')
         ->where('verified',1)
         ->get();
