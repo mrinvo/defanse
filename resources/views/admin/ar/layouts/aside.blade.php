050ccf56c624c0132ed3ccc1c422c293738939fc
mrinvo <huss.mostafa12@gmail.com>
2022-11-23 17:19:01 +0200

    lsdjfldskfj

--- resources/views/admin/ar/layouts/aside.blade.php
+++ resources/views/admin/ar/layouts/aside.blade.php
@@ -16,7 +16,7 @@ $accepted = App\Models\clerk::has('detail')->has('files')->has('families')->wher
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link">
-      <img src="/dashboard/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">وزارة الدفاع</span>
     </a>
@@ -26,7 +26,7 @@ $accepted = App\Models\clerk::has('detail')->has('files')->has('families')->wher
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-            <img src="/1.png" class="img-circle elevation-2" alt="User Image">
+            {{-- <img src="/1.png" class="img-circle elevation-2" alt="User Image"> --}}
         </div>
         <div class="info">
           <a href="#" class="d-block">Admin </a>
